5b2e46839d3c4dcbd3096190bb14bc4c1e83c24f
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-06 01:55:28 +0300

    Add files via upload

---
+++ The average of two numbers.rb
@@ -0,0 +1,9 @@
+number1=421
+puts"Напишите первое число: " +number1.to_s
+number2=124.0
+puts"Напишите второе число: " +number2.to_s
+average=(number1+number2)/2
+puts"Среднее: " +average.to_s
+
+
+
