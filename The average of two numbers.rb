98175d5d5e054fa5228068c1f070cea1fe32e4c3
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-11 15:57:49 +0300

    Update The average of two numbers.rb

--- The average of two numbers.rb
+++ The average of two numbers.rb
@@ -1,7 +1,9 @@
-number1=421
-puts"Напишите первое число: " +number1.to_s
-number2=124.0
-puts"Напишите второе число: " +number2.to_s
+puts"Напишите первое число: "
+number1=gets.to_f
+puts"Первое число: " +number1.to_s
+puts"Напишите второе число: "
+number2=gets.to_i
+puts"Второе число: " +number2.to_s
 average=(number1+number2)/2
 puts"Среднее: " +average.to_s
 
