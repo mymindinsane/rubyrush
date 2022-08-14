ec884f3f0c7e2d4c848a64eb8d34afec892d6936
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-14 04:50:59 +0300

    Add files via upload

---
+++ the average of three numbers.rb
@@ -0,0 +1,8 @@
+puts "Введите первое число: "
+number1=gets.to_f
+puts "Введите второе число: "
+number2=gets.to_f
+puts "Введите третье число: "
+number3=gets.to_f
+average = (number1+number2+number3)/3
+puts "Среднее равно: "+ average.to_s
