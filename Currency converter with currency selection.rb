4321ad7ab8dc6957ddb106d2519dadd0ea885794
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-21 15:24:38 +0300

    Update Currency converter with currency selection.rb

--- Currency converter with currency selection.rb
+++ Currency converter with currency selection.rb
@@ -1,15 +1,15 @@
 puts "Какая у вас сейчас на руках валюта
 1.Рубли
 2.Доллары"
-choice=gets.to_f
-if (choice==1)
+choice=gets.chomp
+if (choice=="1")
   puts "Сколько сейчас стоит один бел. рубль?"
 byn_rate=gets.to_f
 puts "Сколько у вас сейчас бел. рублей?"
 byn_amount=gets.to_f
 total=(byn_amount*byn_rate).round(2)
 puts "У вас будет "+ total.to_s + "$"
-elsif (choice==2)
+elsif (choice=="2")
        puts "Сколько сейчас стоит один доллар?"
        usd_rate=gets.to_f
        puts "Сколько у вас сейчас долларов?"
