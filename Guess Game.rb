5fa087aa865ea9563bb363d652ab2986f9b6f7cd
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-24 15:23:33 +0300

    Update Guess Game.rb

--- Guess Game.rb
+++ Guess Game.rb
@@ -1,25 +1,25 @@
-a=rand(0..15)
+number=rand(0..15)
 for i in(1..3)
   puts "Попробуйте угадать число:"
-  b=gets.to_i
+  guess=gets.to_i
   puts i.to_s + " Попытка"
-  if(a==b)
+  if(number==guess)
     puts"Вы угадали число!"
     exit
-  elsif (a>b)
+  elsif (number>guess)
     puts "Нужно больше"
-    if(a-b).abs<3
+    if(number-guess).abs<3
       puts"Тепло"
     else
       puts "Холодно"
     end
-  elsif (a<b)
+  elsif (number<guess)
     puts"Нужно меньше"
-    if(a-b).abs<3
+    if(number-guess).abs<3
       puts"Тепло"
     else
       puts "Холодно"
     end
   end
 end
-puts "Число было равным: " + a.to_s
+puts "Число было равным: " + number.to_s
