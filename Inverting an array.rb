cc04de409bc488ccbb116c9e3c7bd2d1acd2a0cf
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-26 00:41:10 +0300

    Add files via upload

---
+++ Inverting an array.rb
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5]
+puts "Исходный массив:"
+print numbers.to_s + "\n"
+puts "Исходный массив в обратном порядке:"
+print (numbers.reverse).to_s + "\n"
+puts "Исходный массив(без изменения):"
+print numbers.to_s + "\n"
+puts "Исходный массив(после изменения):"
+print numbers.reverse!
+
