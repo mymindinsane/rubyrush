124d2ff9617a7d15f9ee48b34c468652b2307d1d
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-28 01:04:15 +0300

    Update Rock Paper Scissors.rb

--- Rock Paper Scissors.rb
+++ Rock Paper Scissors.rb
@@ -1,15 +1,13 @@
-massive=['Камень','Ножницы ','Бумага']
+elements=['Камень','Ножницы ','Бумага']
 puts "Выберите 1-Камень 2-Ножницы 3-Бумага"
 player = gets.to_i-1
-if(player>3||player<0)
-  puts"Вы ввели неверное число"
+if(player>elements.length||player<elements.length)
+  puts "Вы ввели неверное число"
   abort
 end
-computer=rand(1..3)-1
-
-puts "Вы выбрали " + massive[player]
-puts "Компьютер выбрал " + massive[computer]
-
+computer=rand(1..3)
+puts "Вы выбрали " + elements[player]
+puts "Компьютер выбрал " + elements[computer]
 if(player==computer)
   puts "Ничья"
 elsif (player==0&&computer==1)
