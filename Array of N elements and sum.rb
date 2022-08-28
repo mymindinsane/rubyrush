fa71592b0ab0e6bd042e14d00a2902eaed473176
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-29 00:04:02 +0300

    Add files via upload

---
+++ Array of N elements and sum.rb
@@ -0,0 +1,12 @@
+array = []
+index = 1
+print "Введите желаемое количество элементов массива:"
+amount = gets.to_i
+sum=0
+while index<=amount
+  array<<index
+  sum+=index
+  index+=1
+end
+print "Массив:" + array.to_s + "\n"
+puts "Сумма элементов массива:" + sum.to_s
